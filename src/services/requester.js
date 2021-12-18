f9431651b003c8c42955869d263e3f9716ae1d2c
Christian-Vasilev <KristianVasilevv@gmail.com>
2021-12-18 11:49:09 +0200

    change requester url

--- src/services/requester.js
+++ src/services/requester.js
@@ -8,7 +8,7 @@ const PATCH = 'PATCH';
 const DELETE = 'DELETE';
 
 const httpClient = axios.create({
-    baseURL: 'process.env.REACT_APP_API_BASE_URL',
+    baseURL: 'https://kvasilev.itodorova.dev/',
     withCredentials: true,
     origin: true,
 });
